095370e2c3883cb39f5ef102e872c6b421beca6c
Leon Wehrhan <mail@leonwehrhan.de>
2023-05-18 00:43:13 +0200

    minor changes

--- init_db.py
+++ init_db.py
@@ -5,15 +5,14 @@ import os
 
 if os.path.exists('recipes/site.db'):
     os.remove('recipes/site.db')
-    db.create_all()
 
-    sample_recipe = Recipe()
-    sample_recipe.title = 'Sample Title'
-    sample_recipe.instructions = '1. aa bb \n 2. bb cc dd'
-    sample_recipe.image = ''
-    sample_recipe.link = ''
-    sample_recipe.ingredients = 'Potato \n Salt \n Pepper'
-    db.session.add(sample_recipe)
-    db.session.commit()
-else:
-    db.create_all()
+db.create_all()
+
+sample_recipe = Recipe()
+sample_recipe.title = 'Sample Title'
+sample_recipe.instructions = '1. aa bb \n 2. bb cc dd'
+sample_recipe.image = ''
+sample_recipe.link = ''
+sample_recipe.ingredients = 'Potato \n Salt \n Pepper'
+db.session.add(sample_recipe)
+db.session.commit()
