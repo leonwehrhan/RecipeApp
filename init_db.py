e0d9488e89c881df5e5fb905e8eed0d5d736f0cf
Leon Wehrhan <mail@leonwehrhan.de>
2023-05-18 00:41:48 +0200

    initialize db with sample recipe

--- init_db.py
+++ init_db.py
@@ -1,9 +1,19 @@
 from recipes import db
+from recipes.models import Recipe
 import os
 
 
 if os.path.exists('recipes/site.db'):
     os.remove('recipes/site.db')
     db.create_all()
+
+    sample_recipe = Recipe()
+    sample_recipe.title = 'Sample Title'
+    sample_recipe.instructions = '1. aa bb \n 2. bb cc dd'
+    sample_recipe.image = ''
+    sample_recipe.link = ''
+    sample_recipe.ingredients = 'Potato \n Salt \n Pepper'
+    db.session.add(sample_recipe)
+    db.session.commit()
 else:
     db.create_all()
