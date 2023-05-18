cbbfa39510fbf764ed04407588cbbb8606ed1324
Leon Wehrhan <mail@leonwehrhan.de>
2023-05-18 22:50:18 +0200

    init_db with more realistic recipe

--- init_db.py
+++ init_db.py
@@ -13,8 +13,7 @@ sample_recipe = Recipe()
 sample_recipe.title = 'Kartoffeln mit Kräuterquark'
 sample_recipe.instructions = '1. Kartoffeln in Wasser kochen. 2. Quark mit Schnittlauch und Petersilie, Pfeffer und Salz verrühren. Ggf. Wasser zugeben. Dann Leinöl einrühren.'
 
-for x in ['1 kg Kartoffeln', '1 kg Quark', 'Schnittlauch', 'Petersilie', 'Salz', 'Pfeffer', 'Leinöl']:
-    j = parse_ingredients(x)
+for x in parse_ingredients('\n'.join(['1 kg Kartoffeln', '1 kg Quark', 'Schnittlauch', 'Petersilie', 'Salz', 'Pfeffer', 'Leinöl'])):
     a = RecipeIngredient(quantity=x[1], unit=x[2])
     a.ingredient = Ingredient(name=x[0])
     sample_recipe.ingredients.append(a)
