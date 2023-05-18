23df735be329d724fd6093c24fe00e7fc59000e5
Leon Wehrhan <mail@leonwehrhan.de>
2023-05-18 17:31:00 +0200

    secret key file

--- recipes/__init__.py
+++ recipes/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from secret_key import get_secret_key
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '53988888f0b3dbcfa810237ab0bdc2b2'
+app.config['SECRET_KEY'] = get_secret_key()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
---
+++ recipes/secret_key.py
@@ -0,0 +1,3 @@
+def get_secret_key():
+    secret_key = '53988888f0b3dbcfa810237ab0bdc2b2'
+    return secret_key
