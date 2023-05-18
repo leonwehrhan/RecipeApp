f824745e9db56049da46f40353ed2c8cc2930447
Leon Wehrhan <mail@leonwehrhan.de>
2023-05-18 20:02:04 +0200

    bugfix

--- recipes/__init__.py
+++ recipes/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from secret_key import get_secret_key
+from recipes import secret_key
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = get_secret_key()
+app.config['SECRET_KEY'] = secret_key.get_secret_key()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
