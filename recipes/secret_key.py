9e16fcdc51741bed74d8be465f280173d5658435
Leon Wehrhan <37343870+leonwehrhan@users.noreply.github.com>
2024-02-29 18:11:41 +0100

    Update secret_key.py

--- recipes/secret_key.py
+++ recipes/secret_key.py
@@ -1,3 +1,6 @@
 def get_secret_key():
+    '''
+    Function that returns a secret key. This is an example key, which is published. Do not use it for any project.
+    '''
     secret_key = '53988888f0b3dbcfa810237ab0bdc2b2'
     return secret_key
